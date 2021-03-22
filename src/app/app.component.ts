efa0879e416aec829cd621541ba111279958c8c2
unknown <ibmjavafsd11@iiht.tech>
2021-03-22 09:22:45 +0530

    added angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {render} from 'creditcardpayments/creditCardPayments';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WorkoutAngular';
+  title = 'WorkoutTrackerUi';
+
+  constructor(){
+    render(
+      {
+        id :"#myPaypalButtons",
+        currency :"INR",
+        value : "100",
+        onApprove: (details) => {
+          alert("Transaction Successful");
+
+        }
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PaymentComponent } from './payment/payment.component';
+import { HomeComponent } from './home/home.component';
+import { VideostreamComponent } from './videostream/videostream.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentComponent,
+    HomeComponent,
+    VideostreamComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
