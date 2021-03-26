6a2c8c9ab510e0ca06741ac53e57d367a3c70f94
Deepthi Sarikonda <sarikonda.deepthi@ibm.com>
2021-03-26 09:28:46 +0530

    adding components which seemed to be deleted

---
+++ fitnessTracker_Angular/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { VideostreamComponent } from './videostream/videostream.component';
+import { PaymentComponent } from './payment/payment.component';
+import { DietplanComponent } from './dietplan/dietplan.component';
+import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+
+
+const routes: Routes = [
+
+  {path : '*', component: AppComponent},
+  {path :'payment', component : PaymentComponent},
+  {path : 'videostream', component : VideostreamComponent},
+  {path : 'dietplan' , component : DietplanComponent},
+  {path : 'modal', component : ModalComponent},
+  {path : 'login', component : LoginComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ fitnessTracker_Angular/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { PaymentComponent } from './payment/payment.component';
+import { VideostreamComponent } from './videostream/videostream.component';
+
+import { DietplanComponent } from './dietplan/dietplan.component';
+import { FormsModule, NgModel } from '@angular/forms';
+import { ModalComponent } from './modal/modal.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaymentComponent,
+    VideostreamComponent,
+    DietplanComponent,
+    ModalComponent,
+    DialogComponent,
+    LoginComponent 
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
